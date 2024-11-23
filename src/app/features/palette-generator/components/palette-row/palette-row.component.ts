cfbd25dbf4df179282ec78b872778938366f9002
Matthieu <matthieu.delarue5@gmail.com>
2024-11-23 00:53:45 -0800

    ✨ Add copy action on color

--- src/app/features/palette-generator/components/palette-row/palette-row.component.ts
+++ src/app/features/palette-generator/components/palette-row/palette-row.component.ts
@@ -1,7 +1,6 @@
 import { CommonModule } from "@angular/common";
 import { Component, effect, input, linkedSignal } from "@angular/core";
-import { GetColorName } from "hex-color-to-color-name";
-
+import chroma from "chroma-js";
 
 @Component({
   selector: "app-palette-row",
@@ -18,5 +17,28 @@ export class PaletteRowComponent {
       console.log(this.palette());
       console.log(this.selectedColor());
     });
+  }
+
+  public getTextColor(bgColor: string): string {
+    const color = chroma(bgColor);
+    const luminance = color.luminance();
+    const textColor = luminance > 0.5 ? color.darken(3).hex() : color.brighten(3).hex();
+
+    return textColor;
+  }
+
+  public getColorNumber(index: number): string {
+    switch (index) {
+      case 0:
+        return "50";
+      case 10:
+        return "950";
+      default:
+        return String(index * 100);
+    }
+  }
+
+  public copyColorToClipboard(color: string): void {
+    navigator.clipboard.writeText(color);
   }
 }

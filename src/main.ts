1c3f2905057b3c963604b0ec5b23dd3e627f9f3c
Matthieu <matthieu.delarue5@gmail.com>
2025-02-15 17:10:41 +0100

    💄 Update title and format code in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,8 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
-import {inject} from "@vercel/analytics"
+import { bootstrapApplication } from "@angular/platform-browser";
+import { appConfig } from "./app/app.config";
+import { AppComponent } from "./app/app.component";
+import { inject } from "@vercel/analytics";
 
-inject()
+inject();
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch(err => console.error(err));
